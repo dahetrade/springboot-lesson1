42f04b413130ad0c7615de658799a386331446f5
dahetrade <dahetrade@sina.com>
2018-10-30 16:36:28 +0800

    Initial commit

---
+++ src/test/java/com/xiek/springboot/springbootlesson1/SpringbootLesson1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xiek.springboot.springbootlesson1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootLesson1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
